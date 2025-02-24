08b9fa8e291e8bebe2688225fe2f1e1526fb1c38
SumitPrusty <sumitprusty85@gmail.com>
2025-02-24 08:07:35 +0530

    first commit

---
+++ src/test/java/com/example/food_order_api/FoodOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.food_order_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
